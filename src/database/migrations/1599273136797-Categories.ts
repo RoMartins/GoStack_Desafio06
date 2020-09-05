66efd50a44fec5c10f4f13c92e7d8e4e22e223cc
RoMartins <rodrigomartins_santos1804@hotmail.com>
2020-09-04 23:33:46 -0300

    Create Database and Transactions Migration

---
+++ src/database/migrations/1599273136797-Categories.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class Categories1599273136797 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<any> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<any> {
+    }
+
+}
